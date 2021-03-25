8eb6fc9c3b769efd9543976ce12fee1faa5e6c7b
JordonGoodsir <jordon.goodsir1@gmail.com>
2021-03-25 17:15:19 +1100

    challenge string ends started

---
+++ stringEnds.ts
@@ -0,0 +1,8 @@
+function solution(str: string, ending: string): boolean { 
+
+    console.log(str.split("").length - ending.split("").length)
+    return false; // TODO: complete
+  } 
+
+  solution('abc', 'bc') // returns true
+
